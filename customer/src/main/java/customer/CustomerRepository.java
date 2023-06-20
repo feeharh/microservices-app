4d4021d5208ab34b991e96480c59d37c1c7d07c9
finzy <finzysewa@gmail.com>
2023-06-20 12:29:05 -0500

    testing

--- customer/src/main/java/customer/CustomerRepository.java
+++ customer/src/main/java/customer/CustomerRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
     Optional<Customer> findCustomerByEmail(String email);
 
+    Optional<Customer> findCustomerById(String id);
+
 }
